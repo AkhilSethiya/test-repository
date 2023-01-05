08c4cf06068ed88156d0b3e7b41ceaf226fb6045
Akhil Sethiya <sethiyaakhil20@gmail.com>
2023-01-05 15:49:40 +0530

    water

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-hello = input("Are you Hungry")
+hello = input("Are you Hungry?")
 if hello == "yes":
 	print("Eat Samosa")
 	print("Eat Pizza")
 	print("Eat Burger")
 else:
-	print("Do your Homework") 
+	thirsty = input("Are you thirsty?")
+	if thirsty == "yes":
+		print("Drink Water") 
